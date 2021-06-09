d91858c0ebb8f6524da99e4f27b777b6bac97cfa
Gilead <gileadgafni4s@gmail.com>
2021-06-09 14:02:21 +0300

    Adding font size change and lines mark+switch

--- js/main.js
+++ js/main.js
@@ -2,11 +2,40 @@ var gElCanvas;
 var gCtx;
 
 function onInit() {
+    loadImages();
     gElCanvas = document.querySelector('.canvas');
     gCtx = gElCanvas.getContext('2d');
+    editMeme();
+}
+
+function loadImages() {
+    var imgs = getImgs();
+    var strHtmls = imgs.map(img =>
+        `<img src="${img.url}" onclick="imgClicked(${img.id})" />`
+
+    );
+    var elImagesGallery = document.querySelector('.images-gallery');
+    elImagesGallery.innerHTML = strHtmls.join('');
+}
+
+function imgClicked(imgId) {
+    var meme = getMeme();
+    meme.selectedImgId = imgId;
+    editMeme();
+    document.querySelector('.editor-box').hidden = false;
+    document.querySelector('.images-gallery').hidden = true;
+
+
+}
+
+function backToGallery() {
+    document.querySelector('.editor-box').hidden = true;
+    document.querySelector('.images-gallery').hidden = false;
+}
+
+function editMeme() {
+
     drawImage();
-
-
 }
 
 function drawImage() {
@@ -30,16 +59,22 @@ function onChangeCanvasText(canvasText) {
 }
 
 function renderCanvas() {
-    gCtx.save()
+    gCtx.save();
     gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height);
     drawImage();
-    gCtx.restore()
+    gCtx.restore();
 }
 
 function addText() {
-    var selectedLine = getMemeText();
+    var memeLines = getMemeLines();
+    var selectedLine = getSelectedLine();
     document.querySelector('[name=canvas-text]').value = selectedLine.txt;
-    drawText(selectedLine.txt, selectedLine.pos, selectedLine.align, selectedLine.color, selectedLine.size);
+    memeLines.forEach(line => {
+        drawText(line.txt, line.pos, line.align, line.color, line.size);
+
+    })
+    markText();
+
 }
 
 function drawText(text, pos, alignment, color, fontSize) {
@@ -53,4 +88,25 @@ function drawText(text, pos, alignment, color, fontSize) {
     gCtx.strokeText(text, x, y);
     gCtx.fillText(text, x, y);
 
+}
+
+function markText() {
+    var selectedLine = getSelectedLine();
+    gCtx.lineWidth = 2;
+    gCtx.strokeRect(20, selectedLine.pos.y - selectedLine.size - 10, 400 + selectedLine.size, selectedLine.size + 30);
+}
+
+function onChangeFontSize(diff) {
+    changeFontSize(diff);
+    renderCanvas();
+}
+
+function onChangeTextPos(yDelta) {
+    changeTextVerticalPos(yDelta);
+    renderCanvas();
+}
+
+function onSwitchSelectedLines() {
+    switchSelectedLines();
+    renderCanvas();
 }
--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,19 +1,33 @@
 var gKeywords = { 'happy': 1, 'funny puk': 1 }
-var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['happy'] }];
+var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['happy'] },
+    { id: 2, url: 'img/2.jpg', keywords: ['happy'] }
+];
 var gMeme = {
     selectedImgId: 1,
     selectedLineIdx: 0,
     lines: [{
-        txt: 'I never eat Falafel',
-        size: 32,
-        align: 'center',
-        color: 'black',
-        pos: { x: 200, y: 40 }
-    }]
+            txt: 'I never eat Falafel',
+            size: 32,
+            align: 'center',
+            color: 'black',
+            pos: { x: 200, y: 50 }
+        },
+        {
+            txt: 'I always eat Falafel',
+            size: 32,
+            align: 'center',
+            color: 'black',
+            pos: { x: 200, y: 480 }
+        }
+    ]
 }
 
 function getMeme() {
     return gMeme;
+}
+
+function getImgs() {
+    return gImgs;
 }
 
 function getMemeImage() {
@@ -22,12 +36,34 @@ function getMemeImage() {
 
 }
 
-function getMemeText() {
+function getMemeLines() {
+    return gMeme.lines;
+}
+
+function getSelectedLine() {
     var selectedLine = gMeme.lines[gMeme.selectedLineIdx];
     return selectedLine;
 }
 
 function changeCanvasText(canvasText) {
-    var selectedLine = getMemeText();
+    var selectedLine = getSelectedLine();
     selectedLine.txt = canvasText;
+}
+
+function changeFontSize(diff) {
+    var selectedLine = getSelectedLine();
+    selectedLine.size += diff;
+}
+
+function changeTextVerticalPos(yDelta) {
+    var selectedLine = getSelectedLine();
+    selectedLine.pos.y += yDelta;
+}
+
+function switchSelectedLines() {
+    if (gMeme.selectedLineIdx < gMeme.lines.length - 1) {
+        gMeme.selectedLineIdx++;
+    } else if (gMeme.selectedLineIdx > 0) {
+        gMeme.selectedLineIdx--;
+    }
 }
